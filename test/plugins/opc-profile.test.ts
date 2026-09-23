0b2eb86ca827034075a018f4efb7f35ba8ed8bdc
WdongW <13246841721@163.com>
2026-09-23 15:15:22 +0800

    release: bundle viral preparation flow

--- test/plugins/opc-profile.test.ts
+++ test/plugins/opc-profile.test.ts
@@ -37,7 +37,7 @@ describe('OPC desktop profile', () => {
       name: '@opc/dsh-feishu-docs', artifact: 'plugins/opc-dsh-feishu-docs-0.1.0.tgz', client: false
     }))
     expect(profile.plugins).toContainEqual(expect.objectContaining({
-      name: '@opc/dsh-viral-chase', version: '0.1.44', artifact: 'plugins/opc-dsh-viral-chase-0.1.44.tgz'
+      name: '@opc/dsh-viral-chase', version: '0.1.45', artifact: 'plugins/opc-dsh-viral-chase-0.1.45.tgz'
     }))
     expect(profile.plugins).toContainEqual(expect.objectContaining({
       name: '@opc/dsh-dev-status-control', version: '0.2.8',
